daf9a81e49bcc7722196475442074db3001d4901
AJ Piter <amanda.piter@gmail.com>
2020-12-21 20:23:18 -0500

    Update Querries.sql

--- Querries.sql
+++ Querries.sql
@@ -1,1 +1,19 @@
-#
+##### Check Data Exists #####
+
+SELECT COUNT(*) AS Variable
+FROM DataFrame.TableName
+WHERE Variable IS NOT NULL
+AND Variable2 IS NOT NULL;
+
+##### Check Data Exists & Filter by Variable #####
+
+SELECT COUNT(*) AS Variable
+FROM DataFrame.TableName
+WHERE Variable IS NOT NULL
+AND Variable2 IS NOT NULL;
+AND Variable3 =1;"
+
+##### Returns the Levels of the Variable #####
+
+SELECT DISTINCT Variable
+FROM DataFrame.TableName;
