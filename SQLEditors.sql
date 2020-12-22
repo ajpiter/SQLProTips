e6fabfc963c03360ab7eb06f5f2968c7f43b8079
AJ Piter <amanda.piter@gmail.com>
2020-12-21 20:30:55 -0500

    Create SQLEditors.sql

---
+++ SQLEditors.sql
@@ -0,0 +1,8 @@
+#MySQL 
+#Open source, and common in academic settings for ease of use and access. 
+
+#Oracle SQL Developer 
+#Common in business settings. Super easy to limit functionality of users. 
+
+#Azure SQL
+#Intergrates easily with PowerBi 
